316d5e24ded1cc44b3b505596711c7c9e0ea10a3
hanss <hanssbcr@gmail.com>
2018-07-31 20:38:59 -0300

    home renovada 90% terminada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Resi Bootstrap Template</title>
+  <title>Beers</title>
 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -15,6 +15,7 @@
   <link href="css/responsive-slider.css" rel="stylesheet">
   <link rel="stylesheet" href="css/animate.css">
   <link rel="stylesheet" href="css/font-awesome.min.css">
+
   <link href="css/stylehome.css" rel="stylesheet">
   <!-- =======================================================
     Theme Name: Resi
@@ -26,36 +27,55 @@
 
 <body>
 
-
-  <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light" style="background-color:#f7f1e3;">
-    <a class="navbar-brand" href="#">Beers</a>
+  <nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light" style="background-color:#303952;">
+    <a class="navbar-brand beerstittle text-white" href="#">Beers</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 
-        <div class="navbar-nav">
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" >
-            {{ csrf_field() }}
-            <button type="submit" name="button"></button>
-        </form>
-          <a class="nav-item nav-link" href="#"><i class="fas fa-envelope"></i></a>
-          <a class="nav-item nav-link" href="#"><i class="fas fa-bell"></i></a>
-          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">Moments</a>
+        <div class="navbar-nav ">
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                 <img src="images/{{ Auth::user()->avatar}}" alt="profile" class="rounded-circle" style="width:35px; height:35px;">  {{-- {{ Auth::user()->name }} --}}
+              </a>
+
+              <ul class="dropdown-menu">
+                  <li>
+                      <a href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                                   document.getElementById('logout-form').submit();">
+                          Logout
+                      </a>
+
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                          {{ csrf_field() }}
+                      </form>
+                  </li>
+              </ul>
+          </li>
+
+          <a class="nav-item nav-link text-white" href="#"><i class="fas fa-envelope"></i></a>
+          <a class="nav-item nav-link text-white" href="#"><i class="fas fa-bell"></i></a>
+          <a class="nav-item nav-link active text-white" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link text-white" href="#">Moments</a>
 
         </div>
+
       </div>
-      <span class="navbar-text">
+
+      <span class="navbar-text text-white m-auto pr-3 font-weight-bold">
         {{ Auth::user()->name }}
       </span>
       <form class="form-inline navbar-right">
-        <button class="btn btn-outline-success" type="button">New Post</button>
+        <button class="btn btn-outline-success mr-3" type="button" data-toggle="modal" data-target="#exampleModalCenter">New Post</button>
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <button class="btn btn-outline-success my-2 my-sm-0 mr-3" type="submit">Search</button>
       </form>
     </div>
   </nav>
+
+
 <div class="container-fluid " style="margin-top:15vh;">
   <div class="col-md-3">
     <div class="card w-100" style="width: 18rem;">
@@ -145,46 +165,24 @@
         <h5 class="card-title">Hace nuevos amigos</h5>
         <h6 class="card-subtitle mb-2 text-muted">Users registrados en Beers</h6>
         <p class="card-text">
-          <ul class="list-group">
+          <ul class="list-group list-group-flush">
             @foreach($usuario as $us)
-            <li class="list-group-item">{{$us->name}}</li>
+            <a href="#" class="list-group-item list-group-item-action">{{$us->name}}</a>
             @endforeach
           </ul>
         </p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+        <a href="#" class="card-link">Buscar Amigo</a>
+
       </div>
     </div>
   </div>
 </div>
-
-    <div class="sub-footer">
-      <div class="container">
+<footer>
+    <div class="sub-footer mt-5">
+      <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-12">
-            <ul class="social-network">
-              <li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook fa-1x"></i></a></li>
-              <li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter fa-1x"></i></a></li>
-              <li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin fa-1x"></i></a></li>
-              <li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest fa-1x"></i></a></li>
-              <li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus fa-1x"></i></a></li>
-            </ul>
-          </div>
-          <div class="col-lg-12">
-            <div class="copyright">
-              <p>&copy;Resi Theme - All right reserved.</p>
-              <div class="credits">
-                <!--
-                  All the links in the footer should remain intact.
-                  You can delete the links only if you purchased the pro version.
-                  Licensing information: https://bootstrapmade.com/license/
-                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Resi
-                -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-              </div>
-            </div>
-          </div>
-
+          <div class="col-md-12" style="background-color: #303952; height:5vh;">
+            <p class="text-center text-light">copyright Beers</p>
         </div>
       </div>
     </div>
@@ -198,7 +196,35 @@
   <script>
     wow = new WOW({}).init();
   </script>
-
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalCenterTitle"><img src="images/{{ Auth::user()->avatar}}" alt="profile" class="rounded-circle" style="width:35px; height:35px;"></h5>
+          <h5 class="ml-3">{{ Auth::user()->name }}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Titulo</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Mensaje:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          <button type="button" class="btn btn-primary">Publicar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,131 +1,14 @@
-
-
   <!DOCTYPE html>
   <html lang="en" dir="ltr">
     <head>
       <meta charset="utf-8">
-      <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
         <link rel="stylesheet" href="/css/stylesProfile.css">
 
-      <title></title>
+      <title>Profile</title>
     </head>
     <body>
-      <div class="wrapper">
-              <div class="container">
-              	<div class="row">
-                      <div class="col-md-12">
-                      <header id="header">
-
-                        <div class="slider">
-                          <div id="about-slider">
-                            <div id="carousel-slider" class="carousel slide" data-ride="carousel">
-                              <!-- Indicators -->
-                              <ol class="carousel-indicators visible-xs">
-                                <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-slider" data-slide-to="1"></li>
-                                <li data-target="#carousel-slider" data-slide-to="2"></li>
-                              </ol>
-
-                              <div class="carousel-inner">
-                                <div class="item active">
-                                  <img src="img/slide1.jpg" class="img-responsive" alt="">
-                                  <div class="carousel-caption">
-                                    <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.3s">
-                                      <h2><span>Clean & Modern Design Template</span></h2>
-                                    </div>
-                                    <div class="col-md-10 col-md-offset-1">
-                                      <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
-                                      </div>
-                                    </div>
-                                    <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.9s">
-                                      <form class="form-inline">
-                                        <div class="form-group">
-                                          <button type="livedemo" name="Live Demo" class="btn btn-primary btn-lg" required="required">Live Demo</button>
-                                        </div>
-                                        <div class="form-group">
-                                          <button type="getnow" name="Get Now" class="btn btn-primary btn-lg" required="required">Get Now</button>
-                                        </div>
-                                      </form>
-                                    </div>
-                                  </div>
-                                </div>
-
-                                <div class="item">
-                                  <img src="img/slide2.jpg" class="img-responsive" alt="">
-                                  <div class="carousel-caption">
-                                    <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.0s">
-                                      <h2>Fully Responsive</h2>
-                                    </div>
-                                    <div class="col-md-10 col-md-offset-1">
-                                      <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.6s">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
-                                      </div>
-                                    </div>
-                                    <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="1.6s">
-                                      <form class="form-inline">
-                                        <div class="form-group">
-                                          <button type="livedemo" name="purchase" class="btn btn-primary btn-lg" required="required">Live Demo</button>
-                                        </div>
-                                        <div class="form-group">
-                                          <button type="getnow" name="subscribe" class="btn btn-primary btn-lg" required="required">Get Now</button>
-                                        </div>
-                                      </form>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-
-                              <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
-                      					<i class="fa fa-angle-left"></i>
-                      				</a>
-
-                              <a class=" right carousel-control hidden-xs" href="#carousel-slider" data-slide="next">
-                      					<i class="fa fa-angle-right"></i>
-                      				</a>
-                            </div>
-                            <!--/#carousel-slider-->
-                          </div>
-                          <!--/#about-slider-->
-                        </div>
-                        <!--/#slider-->
-                      	<nav class="navbar navbar-default">
-                              <!-- Brand and toggle get grouped for better mobile display -->
-                              <div class="navbar-header">
-
-                                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNav">
-                                  <span class="sr-only">Toggle navigation</span>
-                                  <span class="icon-bar"></span>
-                                  <span class="icon-bar"></span>
-                                  <span class="icon-bar"></span>
-                                </button>
-                                <a class="navbar-brand" href="#"><img class="img-responsive" src="https://i.pinimg.com/originals/61/89/ec/6189ec138fb52000004aaf6662c72b2c.jpg"></a>
-                                <span class="site-name"><b>Hanns</b> Solo</span>
-                                <span class="site-description">colgando consolas por ahi</span>
-                              </div>
-
-                              <!-- Collect the nav links, forms, and other content for toggling -->
-                              <div class="collapse navbar-collapse" id="mainNav" >
-                                <ul class="nav main-menu navbar-nav">
-                                  <li><a href="/homepage"><i class="fa fa-home"></i> HOME</a></li>
-                                  <li><a href="#">Tus cervezas</a></li>
-                                  <li><a href="#">Amigos</a></li>
-                                </ul>
-
-                                 <ul class="nav  navbar-nav navbar-right">
-                                  <li><a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
-                                  <li><a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-                                  <li><a href="https://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
-                                </ul>
-                              </div><!-- /.navbar-collapse -->
-      					</nav>
-
-                      </header><!--/#HEADER-->
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     </body>
   </html>
